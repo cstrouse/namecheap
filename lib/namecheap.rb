9bd54e14cd6173cc0e8d529402c9fa4d6643b7aa
Casey Strouse <casey.strouse@gmail.com>
2011-06-20 12:19:52 -0700

    Add registrar lock

--- lib/namecheap.rb
+++ lib/namecheap.rb
@@ -1,4 +1,6 @@
+require 'rubygems'
 require 'httparty'
+require 'yaml'
 
 class NilNamecheapResponse < Exception
 end
@@ -37,24 +39,54 @@ class DomainCheck
   end
 end
 
+class LockCheck
+  attr_accessor :domain, :status
+  
+  def initialize(item)
+    @domain = item["Domain"]
+    @status = item["RegistrarLockStatus"]
+  end
+end
+
 class NamecheapDomainCheckResponse < NamecheapResponse
   def items
     super.collect {|item| DomainCheck.new(item[1])} 
   end
 end
 
+class NamecheapDomainLockStatusResponse < NamecheapResponse
+  def items
+    super.collect {|item| LockCheck.new(item[1])}
+  end
+end
+
+class NamecheapTopLevelDomainResponse < NamecheapResponse
+  def items
+    super.collect {|item| TopLevelDomain.new(item[1])}
+  end
+end
+
 class Namecheap
   attr_reader :username, :key, :client_ip
   def initialize(options = {})
-    config = YAML.load_file("#{File.dirname(__FILE__)}/namecheap.yml").symbolize_keys!
-    @key = options[:key] || config[:key]
-    @username = options[:username] || config[:username]
-    @client_ip = options[:client_ip] || config[:client_ip]
+    config = YAML.load_file("#{File.dirname(__FILE__)}/namecheap.yml")
+    @username = config['username']
+    @client_ip = config['client_ip']
+    @key = config['key']
+  end
+  
+  def is_domain_locked?(domain)
+    # must be a domain associated with your account
+    domain_check_lock_status(domain).items.first.status
+  end
+  
+  def domain_check_lock_status(domain)
+    NamecheapDomainLockStatusResponse.new(do_query("namecheap.domains.getRegistrarLock", "&DomainName=#{domain}"))
   end
 
   def is_domain_available?(domain)
     results = domain_check(domain).items
-    results.nil? ? false : results.first.available?
+    results.nil? ? false : results.first.available
   end
 
   def domain_check(domain)
@@ -64,9 +96,11 @@ class Namecheap
 
   protected
   
-  def do_query(api_method, options)
+  def do_query(api_method, options=nil)
     query = "https://api.sandbox.namecheap.com/xml.response?ApiUser=#{@username}&ApiKey=#{@key}&UserName=#{@username}&ClientIp=#{@client_ip}&Command=#{api_method}"
-    query += options
+    if !options.nil?
+      query += options
+    end
     HTTParty.get(query)
   end
 
